2e467108d1b1dc13abfc8e7b7335914a9f66bfd0
zulin <9683884@qq.com>
2022-06-02 12:33:36 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,26 +7,39 @@ CONNECT=$(adb devices | grep "$IP" -c )
 
 OVOL=20
 
-while [ $CONNECT -lt 1 ];
-do
-        sleep 30
-        echo $CONNECT
-        adb connect $IP
-        CONNECT=$(adb devices | grep $IP -c )
-done
+checkconnect(){
+        while [ $CONNECT -lt 1 ];
+        do
+                sleep 30
+                echo $CONNECT
+                adb connect $IP
+                CONNECT=$(adb devices | grep $IP -c )
+        done
+        checkvol
+}
 
-while :
-do
-        VOL=$(adb shell media volume --get | grep  -E -o "is (\w+)" | awk '{print $2}')
-        DIFVOL=`expr $VOL - $OVOL`
-        OVOL=$VOL
-        echo VOL:$VOL DIFVOL:$DIFVOL
-        if [ $DIFVOL -gt 2 ]; then
-                echo too fast
-                adb shell media volume --show --set 0
-        fi
-        if [ $VOL -gt 20 ]; then
-                echo to larg
-                adb shell media volume --show --set 0
-        fi
-done
+checkvol(){
+        while :
+        do
+                VOL=$(adb shell media volume --get | grep  -E -o "is (\w+)" | awk '{print $2}')
+                if [ $? -ne 0 ]; then
+                        adb disconnect
+                        echo disconnect
+                        break
+                fi
+                DIFVOL=`expr $VOL - $OVOL`
+                OVOL=$VOL
+                echo VOL:$VOL DIFVOL:$DIFVOL
+                if [ $DIFVOL -gt 2 ]; then
+                        echo too fast
+                        adb shell media volume --show --set 0
+                fi
+                if [ $VOL -gt 20 ]; then
+                        echo to larg
+                        adb shell media volume --show --set 0
+                fi
+        done
+        checkconnect
+}
+
+checkconnect
